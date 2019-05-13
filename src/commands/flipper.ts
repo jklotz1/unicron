3e3792ddf546e8021a221749256e7f4b7a5c911e
Kirby Banman <kirby.b@getjobber.com>
2019-05-13 17:03:11 -0600

    make a first stab at case flipping, but break help while I do it.

---
+++ src/commands/flipper.ts
@@ -0,0 +1,38 @@
+import {Command} from '@oclif/command'
+
+export default class Flipper extends Command {
+  static description = 'Takes a single argument as a string and flips its case.';
+  static strict = false;
+
+  static examples = [
+    '$ unicron flipper low \nLOW\n',
+    '$ unicron flipper HIGH \nhigh\n',
+    '$ unicron flipper "a SENTENCE" \nA sentence\n',
+    '$ unicron flipper woweEeEe \nWOWEeEeE\n',
+    '$ unicron flipper multiple strings "to INVERT" \nMULTIPLE\nSTRINGS\nTO invert',
+  ];
+
+  isLowerCase(character: string): boolean {
+    return character === character.toLowerCase();
+  }
+
+  flipCase(toFlip: string): string {
+    let flippedCase = '';
+    for (let i = 0; i < toFlip.length; i++) {
+      if (this.isLowerCase(toFlip[i])) {
+        flippedCase += toFlip[i].toUpperCase();
+      } else {
+        flippedCase += toFlip[i].toLowerCase();
+      }
+    }
+    return flippedCase
+  }
+
+  async run() {
+    const {argv} = this.parse(Flipper);
+
+    argv.forEach((stringToFlip: string) => {
+      this.log(this.flipCase(stringToFlip));
+    })
+  }
+}
